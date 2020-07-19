9e7a2afa2c3be0ff818e281c191f3bb548430f77
Lauryn-gitahi <lauryn.gitahi@gmail.com>
2020-07-19 14:27:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
    const song=document.querySelector(".song");
    const play=document.querySelector(".play");
    const outline=document.querySelector(".moving-outline circle");
-   const video=document.querySelector(".vid-container");
+   const video=document.querySelector(".vid-container video");
 
    //sounds
    const sounds=document.querySelectorAll(".sound-picker button");
